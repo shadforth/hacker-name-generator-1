b92728f516881b8d41025aea5de688e25d2a2a36
SammoWhammo <sam.redman97@gmail.com>
2022-04-28 00:02:30 +0100

    Edit app.run() function

--- src/main.py
+++ src/main.py
@@ -64,6 +64,6 @@ def index():
     return render_template("index.html", name=generate_hacker_name())
 
 
-# Settings for running the app locally for testing.
+# Run the app.
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run()
